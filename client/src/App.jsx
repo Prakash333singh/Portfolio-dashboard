9adf2ec7e6d70bc5c878afa99a5d8f0c3e2556f6
Prakash Adhikari <pa7846665@gmail.com>
2025-08-26 23:28:48 +0530

    some fixes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-// import { useState } from 'react'
 import "tailwindcss";
+
+import Dashboard from "./components/dashboard"
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
 
 function App() {
   return (
-    <div className=''>
-      <div className="text-2xl">
-        <h1>hello</h1>
-        </div>
+    <div >
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/api/stocks" replace />} />
+          <Route path="/api/stocks" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App
