c16ce4cfbff1331c52c349d3a8b02840c41a196a
Prakash Adhikari <pa7846665@gmail.com>
2025-08-26 23:12:29 +0530

    initial tailwind and backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import "tailwindcss";
+
+function App() {
+  return (
+    <div className=''>
+      <div className="text-2xl">
+        <h1>hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
